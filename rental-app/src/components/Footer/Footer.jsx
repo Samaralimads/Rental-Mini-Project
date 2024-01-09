3d691ecb4c1adf0f3dde424ab35c1ebf19061985
evanc <evan.coindreau@gmail.com>
2024-01-09 17:58:29 +0100

    Starting the footer

---
+++ rental-app/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+function Footer() {
+  return (
+    <div>
+      <button
+      onClick={}>
+        Github</button>
+
+    </div>
+  );
+}
+
+export default Footer;
