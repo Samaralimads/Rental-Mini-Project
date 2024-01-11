a48bb6e569067cebf4af470d336632cdf196fbda
Michael Rasolonjatovo <rasolon.michael@gmail.com>
2024-01-11 16:15:17 +0100

    fix global styling

--- rental-app/src/components/NavBar/NavBar.jsx
+++ rental-app/src/components/NavBar/NavBar.jsx
@@ -3,13 +3,13 @@ import "./NavBar.css";
 
 function NavBar() {
   return (
-    <div className="Navbar">
+    <nav className="Navbar">
       <a href="#" className="navLogoContainer" >
         <img src="/logo.png" alt="Rental Logo" className="navLogo" />
       </a>
       <h1>AppRentals</h1>
       <span></span>
-    </div>
+    </nav>
   );
 }
 
--- rental-app/src/components/Sidebar/SideBar.jsx
+++ rental-app/src/components/Sidebar/SideBar.jsx
@@ -3,14 +3,14 @@ import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div className="sidebar">
+    <aside className="sidebar">
       <a href="/">
         <button>HOME</button>
       </a>
       <a href="/about">
         <button>ABOUT</button>
       </a>
-    </div>
+    </aside>
   );
 }
 
