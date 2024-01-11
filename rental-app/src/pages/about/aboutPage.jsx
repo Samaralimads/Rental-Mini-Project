81a31fdc39fd1510e82d8000e6c27275f411651f
“Samara <samara.limads@gmail.com>
2024-01-11 18:03:54 +0100

    about page

--- rental-app/src/pages/about/aboutPage.jsx
+++ rental-app/src/pages/about/aboutPage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./aboutPage.css";
 
-function aboutPage() {
+const AboutPage = () => {
   return (
-    <div>aboutPage</div>
-  )
-}
+    <div className="about-page">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our React Mini project! This page provides information about
+        the project and the team members.
+      </p>
 
-export default aboutPage
+      <h2>Project Description</h2>
+      <p>Describe the project</p>
+
+      <h2>Team Members</h2>
+      {/* <ul>
+        <li>
+          <strong>Team Member 1:</strong> Full Name
+          <br />
+          GitHub:{" "}
+          <a
+            href="https://github.com/username1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          <br />
+          LinkedIn:{" "}
+          <a
+            href="https://www.linkedin.com/in/username1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+     
+      </ul> */}
+    </div>
+  );
+};
+
+export default AboutPage;
